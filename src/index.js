57780e9402d936e8aec35b06833641738a3c598d
hic <incheolh95@gmail.com>
2026-06-24 16:43:33 +0900

    123

---
+++ src/index.js
@@ -0,0 +1,7 @@
+const message = "Hello, Nginx Deployment!";
+
+console.log(message);
+
+document.addEventListener("DOMContentLoaded", () => {
+  document.body.innerHTML = `<h1>${message}</h1>`;
+});
